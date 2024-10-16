5f78d7a3c44aeb2172f9a0863bc5e8182cb5b417
Marco <marco.giacomelli@kinship.io>
2024-10-16 12:14:16 +0200

    Added image download

---
+++ GBRGBDump.Web.Shared/wwwroot/scripts.js
@@ -0,0 +1,7 @@
+﻿function downloadBase64File(fileName, base64String)
+{
+    var link = document.createElement('a');
+    link.href = base64String;
+    link.download = fileName;
+    link.click();
+}
