f82231c102efe4c61e5f3a19953a81c4c60897b2
Marco <marco.giacomelli@kinship.io>
2024-10-18 13:06:32 +0200

    Made datetimes compatible with filenames

---
+++ GBTools.Common/Extensions/Extensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GBTools.Common;
+
+public static class Extensions
+{
+    public static string ToSortableFileName(this DateTime dateTime)
+    {
+        return dateTime.ToString("s").Replace(":", "_");
+    }
+}
