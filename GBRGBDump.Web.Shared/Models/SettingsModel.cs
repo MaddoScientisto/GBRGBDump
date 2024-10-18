fbbf3f62afb1389281842f56745f02415f6645ce
Marco <marco.giacomelli@kinship.io>
2024-10-18 17:15:06 +0200

    Settings Refactoring

--- GBRGBDump.Web.Shared/Models/SettingsModel.cs
+++ GBRGBDump.Web.Shared/Models/SettingsModel.cs
@@ -5,20 +5,34 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace GBRGBDump.Web.Shared.Models
+namespace GBRGBDump.Web.Shared.Models;
+
+public class Settings
 {
-    public class SettingsModel
-    {
-        public string? InputPath { get; set; }
-        public string? OutputPath { get; set; }
+    public SettingsModel Default { get; set; } = new SettingsModel();
 
-        public AverageTypes AverageType { get; set; } = AverageTypes.None;
-        public ChannelOrder ChannelOrder { get; set; } = ChannelOrder.Sequential;
+    public IList<SettingsModel> Presets { get; set; } = [];
 
-        public ScriptModel PreScript { get; set; } = new();
 
-        public ScriptModel PostScript { get; set; } = new();
+}
 
-        public string? PrinterAddress { get; set; }
-    }
+public class SettingsModel
+{
+    public string PresetName { get; set; } = "Default";
+
+    public string? InputPath { get; set; }
+    public string? OutputPath { get; set; }
+
+    public AverageTypes AverageType { get; set; } = AverageTypes.None;
+    public ChannelOrder ChannelOrder { get; set; } = ChannelOrder.Sequential;
+
+    public ScriptModel PreScript { get; set; } = new();
+
+    public ScriptModel PostScript { get; set; } = new();
+
+    public string? PrinterAddress { get; set; } = "http://192.168.7.1";
+
+    public int AebStep { get; set; } = 2;
+
+    public int OutputSize { get; set; } = 1;
 }
