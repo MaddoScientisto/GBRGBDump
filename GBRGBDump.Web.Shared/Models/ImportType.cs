f6aa9b9e233fc44860b122a04129154e7455ec03
Marco <marco.giacomelli@kinship.io>
2024-10-21 12:05:31 +0200

    Wizard for importing from printer

---
+++ GBRGBDump.Web.Shared/Models/ImportType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GBRGBDump.WebShared.Models
+{
+    public enum ImportType
+    {
+        File,
+        Printer,
+        Script
+    }
+}
